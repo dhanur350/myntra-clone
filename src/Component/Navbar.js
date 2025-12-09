7e378ada7a6a2e672f64468c5d56f3c389fd37b7
Dhanur <71970682+dhanur350@users.noreply.github.com>
2025-12-09 15:58:25 +0000

    fix build issues cartitem

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from './myntra.jpg';
 import { useNavigate } from 'react-router-dom';
 import { Button, Dropdown } from 'react-bootstrap'; // Import Dropdown component
 
 function Navbar() {
-  const [cartItems, setCartItems] = useState([]);
+  // const [cartItems, setCartItems] = useState([]);
   const navigate = useNavigate();
 
-  const addToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
+  // const addToCart = (item) => {
+  //   setCartItems([...cartItems, item]);
+  // };
 
   const handleLogout = () => {
     alert("Logged out successfully");
